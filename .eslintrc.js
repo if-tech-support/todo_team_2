f9e5ccdf92e55f04928946598c00ad7014597c8b
Jumpei Asanuma <jun64pei89@gmail.com>
2021-12-20 23:59:24 +0900

    add: initial setting eslint prettier stylelint scss editorConfig
    効率化や統一的なコードを書くためのツールを導入
    scssを使いたい人用の設定も追加
    使わなくてもいいようにコマンドで使うように設定
    
    後々、可能であればpre-commitで自動化したい。

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  ignorePatterns: ['!.eslintrc.js', '!.prettierrc.js'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+}
