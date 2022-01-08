bf0a0e7ed6a565a0ace2e5f877e6dbe0f6de5cc6
london-newyork <cactus1272004@yahoo.co.jp>
2022-01-08 22:01:02 +0900

    #19-priority-first-commit

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <div></div>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
