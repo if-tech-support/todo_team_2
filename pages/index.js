41c8574d45efafd34d5556b56929048581ae7aa0
Erika Yabui <yerika.1127@gmail.com>
2022-01-08 22:28:46 +0900

    #25 新規issue作成

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 
+console.log('test')
+
 export default function Home() {
   return (
     <div className={styles.container}>
