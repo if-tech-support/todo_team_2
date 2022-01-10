af2b89fcdd843752e395db8250f03ef554aa0ab2
london-newyork <cactus1272004@yahoo.co.jp>
2022-01-10 17:00:36 +0900

    #19 Selectbox矢印高さ調整

--- src/components/atoms/search/SearchPriority.jsx
+++ src/components/atoms/search/SearchPriority.jsx
@@ -5,11 +5,11 @@ import { Select } from '@chakra-ui/react';
 export const SearchPriority = () => {
    return (
       <div>
-      <Text fontSize="18px" fontWeight="700" fontFamily="Gothic A1">PRIORITY</Text>
+      <Text fontSize="18px" fontWeight="700" fontFamily="Gothic A1" pb={2}>PRIORITY</Text>
       <Select
       placeholder="- - - - - - -"
       color="#000000"
-      border="1px solid rgba(0, 0, 0, 0.8)" w={48} h={10} borderRadius={2.5} pt={2}>
+      border="1px solid rgba(0, 0, 0, 0.8)" size="md" w={48}>
       <option value="low">Low</option>
       <option value="middle">Middle</option>
       <option value="high">High</option>
