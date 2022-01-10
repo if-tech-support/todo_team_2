07ba8bf2cbfe94a7fef6869c062d9e2ae2d39280
insideplant <kami.10pearl@gmail.com>
2022-01-11 01:23:08 +0900

    resolve conflict

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
+
       </Head>
     </div>
   )
 }
+
