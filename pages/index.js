15515c65886fb9386a5f43ebf16254fefce8157e
後藤 菜央 <nao@gotounoMacBook-Air.local>
2022-01-11 22:40:30 +0900

    #12 検索フォームアイコン追加

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import SearchForm from '../src/components/atoms/search/SearchForm'
-import { SearchPriority } from '../src/components/atoms/search/SearchPriority'
 
 export default function Home() {
   return (
