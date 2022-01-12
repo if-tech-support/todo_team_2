7c8e6ec6afa323801fd20e26fb29f9854864cbed
nao5100000 <nao@gotounoMacBook-Air.local>
2022-01-12 21:12:58 +0900

    #12 検索フォーム 修正

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div>
       <Head>
-        
       </Head>
+        <SearchForm />
     </div>
   )
 }
--- src/components/atoms/search/SearchForm.jsx
+++ src/components/atoms/search/SearchForm.jsx
@@ -1,26 +1,17 @@
 import React　from 'react';
-import { Input,InputGroup,InputRightElement,Text,IconButton } from '@chakra-ui/react';
+import { Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 
 const SearchForm =()=>{
     return(
         <>
             <Text fontSize="18px" fontWeight="700" fontFamily="Gothic A1" pb={2}>SEARCH</Text>
-            <InputGroup w={48}>
+            <InputGroup>
                 <Input
                     placeholder='Text'
                     border="1px solid rgba(0, 0, 0, 0.8)" size="md" 
-                    
                 />
-                <InputRightElement
-                    children={
-                        <IconButton
-                            aria-label='Search database'
-                            icon={<SearchIcon color='rgba(0, 0, 0, 0.2)'/>}
-                            colorScheme='transparent'
-                        />
-                    } 
-                />
+                <InputRightElement children={<SearchIcon color='rgba(0, 0, 0, 0.2)' />} />
             </InputGroup>
         </>
     )
