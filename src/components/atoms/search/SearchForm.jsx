b3fa71ccfe2d0d53da23f961f359167b910a6540
nao5100000 <nao@gotounoMacBook-Air.local>
2022-01-13 00:50:10 +0900

    検索フォーム 整形

--- src/components/atoms/search/SearchForm.jsx
+++ src/components/atoms/search/SearchForm.jsx
@@ -1,20 +1,25 @@
-import React　from 'react';
-import { Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react';
-import { SearchIcon } from '@chakra-ui/icons';
+import React from 'react'
+import { Input, InputGroup, InputRightElement, Text } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
 
-const SearchForm =()=>{
-    return(
-        <>
-            <Text fontSize="18px" fontWeight="700" fontFamily="Gothic A1" pb={2}>SEARCH</Text>
-            <InputGroup>
-                <Input
-                    placeholder='Text'
-                    border="1px solid rgba(0, 0, 0, 0.8)" size="md" 
-                />
-                <InputRightElement children={<SearchIcon color='rgba(0, 0, 0, 0.2)' />} />
-            </InputGroup>
-        </>
-    )
+const SearchForm = () => {
+  return (
+    <>
+      <Text fontSize="18px" fontWeight="700" fontFamily="Gothic A1" pb={2}>
+        SEARCH
+      </Text>
+      <InputGroup>
+        <Input
+          placeholder="Text"
+          border="1px solid rgba(0, 0, 0, 0.8)"
+          size="md"
+        />
+        <InputRightElement
+          children={<SearchIcon color="rgba(0, 0, 0, 0.2)" />}
+        />
+      </InputGroup>
+    </>
+  )
 }
 
-export default SearchForm;
+export default SearchForm
