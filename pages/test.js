1e1062ac6a57296d4bb42db56732a922d099b8f2
insideplant <kami.10pearl@gmail.com>
2022-01-12 20:01:23 +0900

    #16 ファイルのコンポーネント化

--- pages/test.js
+++ pages/test.js
@@ -1,44 +1,7 @@
-import { Table, Tbody,Thead, Th, Tr, Td } from "@chakra-ui/react"
+import TodosTable from "../src/components/organisms/Todo/TodoTable"
 
-export default function TodosTable(){
+export default function (){
   return (
-    <Table size="md">
-      <Thead bg="green.300">
-        <Tr>
-          <Th h={7} width="30%">Task</Th>
-          <Th>Status</Th>
-          <Th>Priority</Th>
-          <Th>Create</Th>
-          <Th>Update</Th>
-          <Th>Action</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        <Tr>
-          <Td>Test</Td>
-          <Td>Test</Td>
-          <Td>Test</Td>
-          <Td>Test</Td>
-          <Td>Test</Td>
-          <Td>Test</Td>
-        </Tr>
-        <Tr>
-          <Td>Test</Td>
-          <Td>Test</Td>
-          <Td>Test</Td>
-          <Td>Test</Td>
-          <Td>Test</Td>
-          <Td>Test</Td>
-        </Tr>
-        <Tr>
-          <Td>Test</Td>
-          <Td>Test</Td>
-          <Td>Test</Td>
-          <Td>Test</Td>
-          <Td>Test</Td>
-          <Td>Test</Td>
-        </Tr>
-      </Tbody>
-    </Table>
+    <TodosTable />
   )
 }
