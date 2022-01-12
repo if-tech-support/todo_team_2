80b00749d55139174a7adeb7600e249d718f8228
Erika Yabui <yerika.1127@gmail.com>
2022-01-12 20:30:28 +0900

    #25 test chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.scss'
 import { ChakraProvider } from '@chakra-ui/react'
+import { NewTodo } from '../newtodo'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <NewTodo />
     </ChakraProvider>
   )
 }
