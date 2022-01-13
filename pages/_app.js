2c4f2d433a56ae8686fb3bb2a6426f41cffed480
Erika Yabui <yerika.1127@gmail.com>
2022-01-13 19:30:52 +0900

    #25 modify New page 1st

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import '../styles/globals.scss'
 import { ChakraProvider } from '@chakra-ui/react'
-import { NewTodo } from '../newtodo'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
-      <NewTodo />
     </ChakraProvider>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
+import { NewTodo } from './NewTodo'
 
 export default function Home() {
   return (
     <div>
       <Head></Head>
+      <NewTodo />
     </div>
   )
 }
