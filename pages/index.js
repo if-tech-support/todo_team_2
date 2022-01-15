2014744e3c0b351c62e68695aec949a56e5b4e89
washogo <aiz4338150@gmail.com>
2022-01-15 23:21:20 +0900

    #18 test.jsxとSearchStatus.jsxを作成

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import Head from 'next/head'
-import TodoListStatus from '../pages/test'
 
 export default function Home() {
   return (
     <div>
       <Head></Head>
-      <TodoListStatus></TodoListStatus>
     </div>
   )
 }
--- pages/test.jsx
+++ pages/test.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import { Select, FormControl, FormLabel } from '@chakra-ui/react'
+import SearchStatus from '../src/components/atoms/search/SearchStatus'
 
-const TodoListStatus = () => {
+const Test = () => {
   return (
-    <FormControl>
-      <FormLabel htmlFor='status' fontSize='18px' fontWeight='bold'>
-        STATUS
-      </FormLabel>
-      <Select id='status' placeholder='- - - - - - -' fontWeight='bold' width='192px' height='40px'>
-        <option>NOT STARTED</option>
-        <option>DOING</option>
-        <option>DONE</option>
-      </Select>
-    </FormControl>
+    <SearchStatus></SearchStatus>
   )
 }
 
-export default TodoListStatus
+export default Test
