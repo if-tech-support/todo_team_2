eb8db7f577d338199384b594629344dedf574785
insideplant <kami.10pearl@gmail.com>
2022-01-15 22:47:55 +0900

    #15 containerを追加

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
 import { Heading } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <>
-      <Heading as="h2" size="lg" my={2}>
-        TODO LIST
-      </Heading>
+      <Container maxW='container.lg'>
+        <Heading as="h2" size="lg" my={2}>
+          TODO LIST
+        </Heading>
+      </Container>
     </>
   )
 }
