4dbb9a7c71550154c1f060f9f86653f32ad05ed6
london-newyork <cactus1272004@yahoo.co.jp>
2022-01-16 17:50:53 +0900

    #57 Backボタンの作成

---
+++ pages/test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BackButton } from '../src/components/atoms/button/BackButton'
+// import { DetailTextarea } from '../src/components/atoms/input/DetailTextarea'
+
+export default function test () {
+    return (
+        <div>
+            <BackButton />
+        </div>
+    )
+}
