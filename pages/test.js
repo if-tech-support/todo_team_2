02ab171468f0262d9c927cbb86643ad1aa9a38fc
london-newyork <cactus1272004@yahoo.co.jp>
2022-01-16 12:01:33 +0900

    #26 Detail のテキストエリア作成

---
+++ pages/test.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import React from 'react'
+import { DetailTextarea } from '../src/components/atoms/input/DetailTextarea'
+
+export default function test () {
+    return (
+        <div>
+            <Head></Head>
+            <DetailTextarea />
+        </div>
+    )
+}
