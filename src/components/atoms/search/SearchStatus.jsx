64da4336cd9fa93def05c35f179ed6d08aa35cde
washogo <aiz4338150@gmail.com>
2022-01-16 13:52:26 +0900

    #18 SearchStatusコンポーネントのレイアウトを微調整

--- src/components/atoms/search/SearchStatus.jsx
+++ src/components/atoms/search/SearchStatus.jsx
@@ -4,10 +4,10 @@ import { Select, FormControl, FormLabel } from '@chakra-ui/react'
 const SearchStatus = () => {
   return (
     <FormControl>
-      <FormLabel htmlFor='status' fontSize='18px' fontWeight='bold'>
+      <FormLabel htmlFor='status' fontFamily='Gothic A1' fontSize='18px' fontWeight='700'>
         STATUS
       </FormLabel>
-      <Select id='status' placeholder='- - - - - - -' fontWeight='bold' width='192px' height='40px'>
+      <Select id='status' placeholder='- - - - - - -' fontWeight='bold' width='192px' height='40px' borderRadius='10px'>
         <option>NOT STARTED</option>
         <option>DOING</option>
         <option>DONE</option>
