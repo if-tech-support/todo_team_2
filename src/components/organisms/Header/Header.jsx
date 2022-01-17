86aa8500bb1e532df772edc24ec3c998eeee2d31
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-01-17 11:55:10 +0000

    #9 Containerコンポーネントを追加して横幅のレイアウトを調整

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,13 +1,15 @@
-import { Box, Flex, Heading, Spacer, Text } from '@chakra-ui/layout'
+import { Box, Container, Flex, Heading, Spacer, Text } from '@chakra-ui/layout'
 
 const Header = () => {
   return (
     <Box bg={'green.300'} mb="16px">
-      <Flex width="1080px" mx="auto" py="12px" alignItems="center">
-        <Heading fontSize="5xl">TODO</Heading>
-        <Spacer />
-        <Text>2022/01/01</Text>
-      </Flex>
+      <Container maxW="container.lg">
+        <Flex mx="auto" py="12px" alignItems="center">
+          <Heading fontSize="5xl">TODO</Heading>
+          <Spacer />
+          <Text>2022/01/01</Text>
+        </Flex>
+      </Container>
     </Box>
   )
 }
