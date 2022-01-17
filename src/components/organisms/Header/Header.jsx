bad7acc510e4b0d3a599bda07174f7669fa829c6
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-01-17 15:39:30 +0000

    #9 Headerコンポーネントのmarginを削除、Containerのmarginを削除

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -2,9 +2,9 @@ import { Box, Container, Flex, Heading, Spacer, Text } from '@chakra-ui/layout'
 
 const Header = () => {
   return (
-    <Box bg={'green.300'} mb="16px">
+    <Box bg={'green.300'}>
       <Container maxW="container.lg">
-        <Flex mx="auto" py="12px" alignItems="center">
+        <Flex py="12px" alignItems="center">
           <Heading fontSize="5xl">TODO</Heading>
           <Spacer />
           <Text>2022/01/01</Text>
