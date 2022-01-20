8a0f791622d0b8abbd88463650535abe41aa56b2
washogo <aiz4338150@gmail.com>
2022-01-20 20:53:46 +0900

    #18 SearchStatus.jsxのコード整形

--- src/components/atoms/search/SearchStatus.jsx
+++ src/components/atoms/search/SearchStatus.jsx
@@ -4,10 +4,22 @@ import { Select, FormControl, FormLabel } from '@chakra-ui/react'
 const SearchStatus = () => {
   return (
     <FormControl>
-      <FormLabel htmlFor='status' fontFamily='Gothic A1' fontSize='18px' fontWeight='700'>
+      <FormLabel 
+        htmlFor='status' 
+        fontFamily='Gothic A1' 
+        fontSize='18px' 
+        fontWeight='700'
+      >
         STATUS
       </FormLabel>
-      <Select id='status' placeholder='- - - - - - -' fontWeight='bold' width='192px' height='40px' borderRadius='10px'>
+      <Select 
+        id='status'
+        placeholder='- - - - - - -' 
+        fontWeight='bold' 
+        width='192px' 
+        height='40px'
+        borderRadius='10px'
+      >
         <option>NOT STARTED</option>
         <option>DOING</option>
         <option>DONE</option>
