2a4ef4b4b17fbdd26f35af7fcf8058ee27808123
nao5100000 <nao@gotounoMacBook-Air.local>
2022-02-01 21:51:21 +0900

    #13 chakra-pagination UI 追加修正

--- src/components/molecules/Pagination.jsx
+++ src/components/molecules/Pagination.jsx
@@ -6,7 +6,7 @@ import { Paginator, Previous, Next, PageGroup } from 'chakra-paginator'
 const Pagination = () => {
   const pagesQuantity = 6
 
-  /* the styles for the unselected page buttons */
+  /* 通常　buttonスタイル  */
   const normalStyles = {
     w: 8,
     h: 8,
@@ -19,7 +19,7 @@ const Pagination = () => {
     },
   }
 
-  /* the styles for the unselected page buttons */
+  /* 選択中 buttonスタイル */
   const activeStyles = {
     w: 8,
     h: 8,
@@ -29,31 +29,22 @@ const Pagination = () => {
       bg: 'green.300',
     },
   }
-  const separatorStyles = {
-    w: 8,
-    h: 8,
-    bg: 'green.200',
-  }
-
   return (
-    <ChakraProvider>
-      <Paginator
-        activeStyles={activeStyles}
-        normalStyles={normalStyles}
-        separatorStyles={separatorStyles}
-        pagesQuantity={pagesQuantity}
-      >
-        <HStack spacing="20px" p={4}>
-          <Previous w={8} h={8} bg="White" border="1px solid Black">
-            <ChevronLeftIcon color="gray.400" w={5} h={5} />
-          </Previous>
-          <PageGroup isInline align="center" spacing="10px" />
-          <Next w={8} h={8} bg="White" border="1px solid Black">
-            <ChevronRightIcon color="gray.400" w={5} h={5} />
-          </Next>
-        </HStack>
-      </Paginator>
-    </ChakraProvider>
+    <Paginator
+      activeStyles={activeStyles}
+      normalStyles={normalStyles}
+      pagesQuantity={pagesQuantity}
+    >
+      <HStack spacing="20px" p={4}>
+        <Previous w={8} h={8} bg="White" border="1px solid Black">
+          <ChevronLeftIcon color="gray.400" w={5} h={5} />
+        </Previous>
+        <PageGroup isInline align="center" spacing="10px" />
+        <Next w={8} h={8} bg="White" border="1px solid Black">
+          <ChevronRightIcon color="gray.400" w={5} h={5} />
+        </Next>
+      </HStack>
+    </Paginator>
   )
 }
 
