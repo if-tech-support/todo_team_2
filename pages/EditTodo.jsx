4a509aeab2d628658366fc164015b1bd5cd09dc2
insideplant <kami.10pearl@gmail.com>
2022-02-01 20:27:07 +0900

    #68 priority初期値を修正

--- pages/EditTodo.jsx
+++ pages/EditTodo.jsx
@@ -4,78 +4,75 @@ import { Stat, StatLabel, StatNumber } from '@chakra-ui/stat'
 import Header from '../src/components/organisms/Header/Header'
 import React from 'react'
 import { DetailTextarea } from '../src/components/atoms/input/DetailTextarea'
-import Title from "../src/components/atoms/input/Title"
-import { BackButton } from "../src/components/atoms/button/BackButton"
+import Title from '../src/components/atoms/input/Title'
+import { BackButton } from '../src/components/atoms/button/BackButton'
 
 export default function EditTodo() {
-    return (
-        <div>
-            <Header />
-            <Box px="100px" pt={4}>
-                <Flex justifyContent="space-between" alignItems="center" pb={5}>
-                    <Text
-                    as="h1"
-                    fontWeight="bold"
-                    fontSize="28px">
-                        EDIT TODO
-                    </Text>
-                    <BackButton />
-                </Flex>
-                <Title />
-                <DetailTextarea />
-                <Flex justifyContent="space-between" pt={5} alignItems="center">
-                    <Flex justifyContent="space-between" w="368px">
-                        <Stat>
-                            <StatLabel
-                                fontSize="16px"
-                                color="gray.800"
-                                lineHeight="1.1"
-                                fontWeight="medium"
-                            >
-                                Created
-                            </StatLabel>
-                            <StatNumber
-                                fontSize="18px"
-                                color="gray.800"
-                                lineHeight="1.1"
-                                fontWeight="medium"
-                            >
-                                2022-01-01&nbsp;&nbsp;18:55
-                            </StatNumber>
-                        </Stat>
-                        <Stat>
-                            <StatLabel
-                                fontSize="16px"
-                                lineHeight="1.1"
-                                fontWeight="medium"
-                                color="gray.800"
-                            >
-                                Updated
-                            </StatLabel>
-                            <StatNumber
-                                fontSize="18px"
-                                lineHeight="1.1"
-                                fontWeight="medium"
-                                color="gray.800"
-                            >
-                                2022-01-01&nbsp;&nbsp;18:55
-                            </StatNumber>
-                        </Stat>
-                    </Flex>
-                    <Button
-                        backgroundColor="green.600"
-                        borderRadius="50px"
-                        border="1px solid"
-                        borderColor="rgba(0, 0, 0, 0.8)"
-                        color="white"
-                        w="112px"
-                        h="40px"
-                        _hover={{ backgroundColor: "green.400" }}
-                    >
-                        UPDATE
-                    </Button>
-                </Flex>
-            </Box>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Box px="100px" pt={4}>
+        <Flex justifyContent="space-between" alignItems="center" pb={5}>
+          <Text as="h1" fontWeight="bold" fontSize="28px">
+            EDIT TODO
+          </Text>
+          <BackButton />
+        </Flex>
+        <Title />
+        <DetailTextarea />
+        <Flex justifyContent="space-between" pt={5} alignItems="center">
+          <Flex justifyContent="space-between" w="368px">
+            <Stat>
+              <StatLabel
+                fontSize="16px"
+                color="gray.800"
+                lineHeight="1.1"
+                fontWeight="medium"
+              >
+                Created
+              </StatLabel>
+              <StatNumber
+                fontSize="18px"
+                color="gray.800"
+                lineHeight="1.1"
+                fontWeight="medium"
+              >
+                2022-01-01&nbsp;&nbsp;18:55
+              </StatNumber>
+            </Stat>
+            <Stat>
+              <StatLabel
+                fontSize="16px"
+                lineHeight="1.1"
+                fontWeight="medium"
+                color="gray.800"
+              >
+                Updated
+              </StatLabel>
+              <StatNumber
+                fontSize="18px"
+                lineHeight="1.1"
+                fontWeight="medium"
+                color="gray.800"
+              >
+                2022-01-01&nbsp;&nbsp;18:55
+              </StatNumber>
+            </Stat>
+          </Flex>
+          <Button
+            backgroundColor="green.600"
+            borderRadius="50px"
+            border="1px solid"
+            borderColor="rgba(0, 0, 0, 0.8)"
+            color="white"
+            w="112px"
+            h="40px"
+            _hover={{ backgroundColor: 'green.400' }}
+          >
+            UPDATE
+          </Button>
+        </Flex>
+      </Box>
+    </div>
+  )
 }
--- pages/Newtodo.jsx
+++ pages/Newtodo.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Router from 'next/router'
 
 import { Stack, Text, Box, Flex, Spacer, HStack } from '@chakra-ui/react'
-// import { BackButton } from '../src/components/atoms/button/BackButton'
+import  BackButton from '../src/components/atoms/button/BackButton'
 import { DetailTextarea } from '../src/components/atoms/input/DetailTextarea'
 import Title from '../src/components/atoms/input/Title'
 import { DraftButton } from '../src/components/atoms/button/DraftButton'
@@ -22,7 +22,7 @@ export default function NewTodo() {
 
   const [title, setTitle] = useState('')
   const [detail, setDetail] = useState('')
-  const [priority, setPriority] = useState('High')
+  const [priority, setPriority] = useState('')
 
   // 現在のTodosの要素数を取得
   const quantity = Object.keys(todos).length
@@ -61,7 +61,7 @@ export default function NewTodo() {
               NEW TODO
             </Text>
             <Spacer />
-            {/* <BackButton /> */}
+            <BackButton />
           </Flex>
           <Title title={title} setTitle={setTitle} />
           <DetailTextarea detail={detail} setDetail={setDetail} />
