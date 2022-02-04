5c9f59eb5b279c50c1e348ce0ee58817b1f55eda
washogo <aiz4338150@gmail.com>
2022-02-05 03:59:52 +0900

    Revert "#72 CommentModal.jsxとcomment.jsxにstate管理と一覧表示を追加"
    This reverts commit 97fa2901dbfc17003b26bfc1a3f9271ffc45a2d9.

--- src/components/atoms/comment/comment.jsx
+++ src/components/atoms/comment/comment.jsx
@@ -1,21 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Box, Flex, Text } from '@chakra-ui/react'
-import { useRecoilValue } from 'recoil'
-import { commentState } from '../../../hooks/CommentsState'
 
-export default function Comment() {
-  const currentComments = useRecoilValue(commentState)
-  console.log(currentComments)
+export default function comment() {
   return (
     <>
-    {currentComments.map((comment) => (
       <Box
         borderRadius="8px"
         border="1px"
         borderColor="Black"
         w="400px"
         h="100px"
-        key={comment.id}
       >
         <Flex
           bg="green"
@@ -27,14 +21,13 @@ export default function Comment() {
           align="center"
           justifyContent="space-between"
         >
-          <Text>{comment.name}</Text>
-          <Text>{comment.createdAt}</Text>
+          <Text>name</Text>
+          <Text>date</Text>
         </Flex>
         <Box pr={4} pl={4}>
-          <Text>{comment.comment}</Text>
+          <Text>text</Text>
         </Box>
       </Box>
-    ))}
     </>
   )
 }
--- src/components/organisms/Modal/CommentModal.jsx
+++ src/components/organisms/Modal/CommentModal.jsx
@@ -1,8 +1,4 @@
-import React, { useState }from 'react'
-import { useRecoilState } from 'recoil'
-import { commentState, dateState } from '../../../hooks/CommentsState'
-import { getTime } from '../../../utils/Now'
-
+import React from 'react'
 import {
   Button,
   Modal,
@@ -19,52 +15,11 @@ import {
 } from '@chakra-ui/react'
 
 export default function CommentModal() {
-
-  const [name, setName] = useState("")
-  const [comment, setComment] = useState("")
-  const [comments, setComments] = useRecoilState(commentState)
-
-  const { currentTime } = getTime()
-  const [times, setTimes] = useRecoilState(dateState)
-  const [id, setId] = useState(0)
-
-  const onAddComment = () => {
-    setTimes([
-      ...times,
-      currentTime,
-    ])
-    setComments(
-      [
-        ...comments,
-        { id: times.length + 1, name: name, comment: comment, createdAt: currentTime},
-      ]
-    )
-    onClose()
-    setName("")
-    setComment("")
-    // setComments([])
-    // setTimes([])
-  }
-
   const { isOpen, onOpen, onClose } = useDisclosure()
   const initialRef = React.useRef()
-
   return (
-    <>
-      <Button
-        backgroundColor="green.600"
-        borderRadius="50px"
-        border="1px solid"
-        borderColor="rgba(0, 0, 0, 0.8)"
-        color="white"
-        w="112px"
-        h="40px"
-        variant="solid"
-        _hover={{ backgroundColor: 'green.400' }}
-        onClick={onOpen}
-      >
-        Comment
-      </Button>
+    <div>
+      <Button onClick={onOpen}>Open Modal</Button>
       <Modal initialFocusRef={initialRef} isOpen={isOpen}>
         <ModalOverlay />
         <ModalContent>
@@ -72,39 +27,21 @@ export default function CommentModal() {
           <ModalBody>
             <FormControl>
               <FormLabel fontSize="lg">Name</FormLabel>
-              <Input
-                placeholder="Name"
-                ref={initialRef}
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
+              <Input placeholder="Name" ref={initialRef} />
             </FormControl>
             <FormControl mt={4}>
               <FormLabel fontSize="lg">Your Comment</FormLabel>
-              <Textarea
-                height="auto"
-                rows={7}
-                placeholder="Your Comment"
-                type="text"
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-              />
+              <Textarea height="auto" rows={7} placeholder="Your Comment" />
             </FormControl>
           </ModalBody>
           <ModalFooter>
-            <Button
-              color="white"
-              bg="green.600"
-              mr={3}
-              onClick={onAddComment}
-            >
+            <Button color="white" bg="green.600" mr={3} onClick={onClose}>
               CREATE
             </Button>
             <Button onClick={onClose}>CANCEL</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
-    </>
+    </div>
   )
 }
