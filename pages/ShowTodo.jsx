6c0da5e63baaa06912912b5669c53f615bac97ce
washogo <aiz4338150@gmail.com>
2022-02-05 03:52:35 +0900

    #72 ShowTodo.jsxにコメント機能を統合

--- pages/ShowTodo.jsx
+++ pages/ShowTodo.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {
-  Box,
   Container,
   Flex,
   Heading,
@@ -13,149 +12,34 @@ import Header from '../src/components/organisms/Header/Header'
 import DetailCard from '../src/components/organisms/Todo/DetailCard'
 import BackButton from '../src/components/atoms/button/BackButton'
 import CommentButton from '../src/components/atoms/button/CommentButton'
+import Comment from '../src/components/atoms/comment/Comment'
 
 function index() {
   return (
     <>
-      <Box>
-        <Header />
-        <Container maxW="container.lg">
-          <Flex py="12px">
-            {/* 以下には現在プルリク中のHeadline.jsxが入ります*/}
-            <Heading as="h3" size="lg">
-              SHOW TODO
-            </Heading>
-            {/* 以上には現在プルリク中のHeadline.jsxが入ります*/}
+      <Header />
+      <Container maxW="container.lg">
+        <Flex py="12px">
+          {/* 以下には現在プルリク中のHeadline.jsxが入ります*/}
+          <Heading as="h3" size="lg">
+            SHOW TODO
+          </Heading>
+          {/* 以上には現在プルリク中のHeadline.jsxが入ります*/}
+          <Spacer />
+          <Flex width="25%">
+            <CommentButton />
             <Spacer />
-            <Flex width="25%">
-              <CommentButton />
-              <Spacer />
-              <BackButton />
-            </Flex>
+            <BackButton />
           </Flex>
-          <HStack spacing={1}>
-            <DetailCard />
-            {/* 以下はダミーのコメント一覧 */}
-            <VStack pt="5" h="480" w="xl">
-              <Box
-                borderRadius="15px"
-                border="1px"
-                borderColor="blackAlpha.800"
-                width="full"
-                height="full"
-              >
-                <Flex
-                  bg="green.600"
-                  px={4}
-                  py={1}
-                  borderRadius="15px 15px 0 0"
-                  borderBottom="1px"
-                  borderColor="blackAlpha.800"
-                >
-                  <Heading as="h5" size="sm" color="white">
-                    ジョン
-                  </Heading>
-                  <Spacer />
-                  <Heading as="h5" size="sm" color="white">
-                    2022/01/01
-                  </Heading>
-                </Flex>
-                <Box px={4}>
-                  <Text>2日後までに完了お願いします。</Text>
-                </Box>
-              </Box>
-              <Spacer />
-              <Box
-                borderRadius="15px"
-                border="1px"
-                borderColor="blackAlpha.800"
-                width="full"
-                height="full"
-              >
-                <Flex
-                  bg="green.600"
-                  px={4}
-                  py={1}
-                  borderRadius="15px 15px 0 0"
-                  borderBottom="1px"
-                  borderColor="blackAlpha.800"
-                >
-                  <Heading as="h5" size="sm" color="white">
-                    リンゴ
-                  </Heading>
-                  <Spacer />
-                  <Heading as="h5" size="sm" color="white">
-                    2022/01/01
-                  </Heading>
-                </Flex>
-                <Box px={4}>
-                  <Text>
-                    内容確認いたしました。修正点メールしましたのでご確認ください。
-                  </Text>
-                </Box>
-              </Box>
-              <Spacer />
-              <Box
-                borderRadius="15px"
-                border="1px"
-                borderColor="blackAlpha.800"
-                width="full"
-                height="full"
-              >
-                <Flex
-                  bg="green.600"
-                  px={4}
-                  py={1}
-                  borderRadius="15px 15px 0 0"
-                  borderBottom="1px"
-                  borderColor="blackAlpha.800"
-                >
-                  <Heading as="h5" size="sm" color="white">
-                    ポール
-                  </Heading>
-                  <Spacer />
-                  <Heading as="h5" size="sm" color="white">
-                    2022/01/01
-                  </Heading>
-                </Flex>
-                <Box px={4}>
-                  <Text>2日後までに完了お願い致します。</Text>
-                </Box>
-              </Box>
-              <Spacer />
-              <Box
-                borderRadius="15px"
-                border="1px"
-                borderColor="blackAlpha.800"
-                width="full"
-                height="full"
-              >
-                <Flex
-                  bg="green.600"
-                  px={4}
-                  py={1}
-                  borderRadius="15px 15px 0 0"
-                  borderBottom="1px"
-                  borderColor="blackAlpha.800"
-                >
-                  <Heading as="h5" size="sm" color="white">
-                    ジョージ
-                  </Heading>
-                  <Spacer />
-                  <Heading as="h5" size="sm" color="white">
-                    2022/01/01
-                  </Heading>
-                </Flex>
-                <Box px={4}>
-                  <Text>Let it be</Text>
-                </Box>
-              </Box>
-            </VStack>
-          </HStack>
-          {/* 以上はダミーのコメント一覧 */}
-          {/* ここにページネーションが入ります */}
-        </Container>
-      </Box>
+        </Flex>
+        <HStack spacing={1}>
+          <DetailCard />
+          <VStack pb="5" h="480" w="xl">
+          <Comment/>
+          </VStack>
+        </HStack>
+        {/* ここにページネーションが入ります */}
+      </Container>
     </>
   )
 }
