bbb25ae9b695cdef5a3ff1435f484f9156573b30
miho <m8na21@yahoo.co.jp>
2022-02-07 15:36:22 +0900

    ダミー削除、recoil記述の修正

--- pages/EditTodo.jsx
+++ pages/EditTodo.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useRecoilState } from 'recoil'
+import { useRecoilValue } from 'recoil'
 import { Box, Flex } from '@chakra-ui/layout'
 import Header from '../src/components/organisms/Header/Header'
 import { DetailTextarea } from '../src/components/atoms/input/DetailTextarea'
@@ -11,26 +11,15 @@ import { UpdateTime } from '../src/components/molecules/timeStamp/UpdateTime'
 import { Headline } from '../src/components/atoms/headline/Headline'
 import { editTodoState } from '../src/hooks/EditTodoState'
 
-// 以下はダミーの編集したいtodo
-const todo = {
-  id: 1,
-  title: '掃除',
-  detail: 'キッチンの換気扇の油落とす',
-  priority: 'High',
-  status: 'not_started',
-  created_day: '2022-01-12 10:40',
-}
-
 export default function EditTodo() {
   // EditTodoState.jsで定義したeditTodoのstateと更新関数を呼び出す
-  const [editTodo, setEditTodo] = useRecoilState(editTodoState)
+  const editTodo = useRecoilValue(editTodoState)
   // todoのタイトルと詳細のstateをローカルに定義
   const [title, setTitle] = useState('')
   const [detail, setDetail] = useState('')
 
   // EditTodo画面のロード時に、編集したいtodoの情報をローカルなstateに保持させる
   useEffect(() => {
-    console.log('useEffect発動！')
     setTitle(() => editTodo.title)
     setDetail(() => editTodo.detail)
   }, [])
@@ -47,10 +36,10 @@ export default function EditTodo() {
         <DetailTextarea detail={detail} setDetail={setDetail} />
         <Flex justifyContent="space-between" pt={5} alignItems="center">
           <Flex justifyContent="space-between" w="368px">
-            <CreationTime />
-            <UpdateTime />
+            <CreationTime created_day={editTodo.created_day} />
+            <UpdateTime updated_day={editTodo.updated_day} />
           </Flex>
-          <UpdateButton />
+          <UpdateButton title={title} detail={detail} />
         </Flex>
       </Box>
     </div>
