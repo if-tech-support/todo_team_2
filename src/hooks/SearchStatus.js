58644022fb2978e6669a72122376579c1ebb661c
washogo <aiz4338150@gmail.com>
2022-02-26 21:08:37 +0900

    #105 recoil-persistの削除

--- src/hooks/SearchStatus.js
+++ src/hooks/SearchStatus.js
@@ -1,7 +1,4 @@
 import { atom } from 'recoil'
-import { recoilPersist } from 'recoil-persist'
-
-const { persistAtom } = recoilPersist();
 
 export const searchPriorityState = atom({
   key: 'searchPriority',
